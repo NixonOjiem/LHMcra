6a95ac41f6e72e1f5967cb61b91e65b9d34708ed
arkstudiodevelopers <nicksonogm@gmail.com>
2025-03-08 19:08:55 +0300

    Initial commit with the components and navigation folders set up

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FlashSale from '../components/FlashSale'
+import NavigationBar from '../components/NavigationBar'
+
+function HomePage() {
+  return (
+    <div>
+        <FlashSale />
+        <NavigationBar />
+    </div>
+  )
+}
+
+export default HomePage
